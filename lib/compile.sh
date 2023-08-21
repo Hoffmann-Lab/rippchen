af6adcd6b9b1a517d38681a9c9c21e1931676e11
koriege <koriege@gmail.com>
2023-08-21 18:59:27 +0200

    simplified setup

--- lib/compile.sh
+++ lib/compile.sh
@@ -1,14 +1,15 @@
 #! /usr/bin/env bash
 # (c) Konstantin Riege
 
-compile::bashbone() {
-	local insdir threads version bashboneversion src="$(dirname "$(readlink -e "$0")")"
+function compile::bashbone(){
+	local insdir threads cfg version bashboneversion src="$(dirname "$(dirname "$(readlink -e "$0")")")"
 	commander::printinfo "installing bashbone"
-	compile::_parse -r insdir -s threads "$@"
+	compile::_parse -r insdir -s threads -f cfg "$@"
 	source "$src/lib/version.sh"
 	rm -rf "$insdir/bashbone-$version"
 	mkdir -p "$insdir/bashbone-$version"
 	cp -r "$src"/* "$insdir/bashbone-$version"
+	rm -f "$insdir/bashbone-$version/scripts/"+(setup|test).sh
 	mkdir -p "$insdir/latest"
 	ln -sfn "$insdir/bashbone-$version" "$insdir/latest/bashbone"
 
@@ -16,18 +17,14 @@ compile::bashbone() {
 	src="$(dirname "$src")"
 
 	commander::printinfo "installing rippchen"
-	compile::_parse -r insdir -s threads "$@"
 	source "$src/lib/version.sh"
 	rm -rf "$insdir/rippchen-$version"
 	mkdir -p "$insdir/rippchen-$version"
-	cp -r "$src"/!(bashbone|setup*) "$insdir/rippchen-$version"
+	cp -r "$src"/!(bashbone) "$insdir/rippchen-$version"
+	rm -f "$insdir/rippchen-$version/scripts/"+(setup|test).sh
 	mkdir -p "$insdir/latest"
 	ln -sfn "$insdir/rippchen-$version" "$insdir/latest/rippchen"
 	ln -sfn "$insdir/bashbone-$bashboneversion" "$insdir/rippchen-$version/bashbone"
 
 	return 0
-}
-
-compile::rippchen() {
-	compile::bashbone
 }
